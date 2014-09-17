968174f663181589ded4b9dffb3864b21c130be6
Justin Anderson <janderson@janderson-mbp.home>
2014-09-16 20:51:13 -0700

    End of Day commit 16Sep14

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -34,5 +34,9 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = if (money==0) 1 
+                                                       else if (money<0) 0 
+                                                       else if (coins.isEmpty) 0 
+                                                       else countChange(money-coins.head,coins) + countChange(money, coins.tail)
+  
 }
